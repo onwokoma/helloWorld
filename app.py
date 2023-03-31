c89d036ab28d9e96f84f0814db288f7444e4213c
Olivia Nwokoma <onwokoma@terpmail.umd.edu>
2023-03-30 23:46:29 -0400

    Created routes to 'favorite course' and 'contact form' html pages. Updated favorite course route to allow data to be passed through from parent template.

--- app.py
+++ app.py
@@ -15,23 +15,16 @@ def hello():  # put application's code here
 def favorite_course():
     print('Subject entered: ' + request.args.get('subject_name'))
     print('Course number entered: ' + request.args.get('course_number'))
-    fun_courses = ['BMGT301', 'BMGT302', 'BMGT402', 'BMGT403']
+    more_courses = ['BMGT301', 'BMGT302', 'BMGT402', 'BMGT403']
 
-    return render_template('favorite-course.html', courses=fun_courses)
+    return render_template('favorite-course.html', courses=more_courses)
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
-        print('Thank you!')
-        print('First name entered: ' + request.form.get('first_name'))
-        print('Last name entered: ' + request.form.get('last_name'))
-        print('Email entered: ' + request.form.get('email'))
-
-        # used to check if the checkbox was checked. need this or else the field is not passed back.
-        if request.form.get('agree_check'):
-            print('Agree to be contacted entered: ' + request.form.get('agree_check'))
-
-    return render_template('contact.html')
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
